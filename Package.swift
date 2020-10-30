32c8ab46a165dc1524588966248f4b3def1eadc8
Andrey Antropov <antropov@worki.ru>
2020-10-30 12:39:41 +0300

    Added Package.swift file

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SHSPhoneComponent",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SHSPhoneComponent",
+            targets: ["SHSPhoneComponent"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "SHSPhoneComponent",
+            dependencies: [
+            ],
+            path: "SHSPhoneComponents/Library",
+            publicHeadersPath: "SHSPhoneComponents/SHSPhoneComponent/SHSPhoneComponent.h"
+        )
+    ]
+)
